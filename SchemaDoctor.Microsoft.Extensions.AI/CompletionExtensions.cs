0e6c5045be080274060cc95475ae14dda0439f87
Magne Helleborg <magne.helleborg@gmail.com>
2025-01-21 10:04:17 +0100

    Docs

--- SchemaDoctor.Microsoft.Extensions.AI/CompletionExtensions.cs
+++ SchemaDoctor.Microsoft.Extensions.AI/CompletionExtensions.cs
@@ -7,13 +7,12 @@ public static class CompletionExtensions
 {
     /// <summary>
     /// Mapper that tries to fix hallucinations if present
-    /// Will lean on the default mapper if possible, and fall back to hacky parsing
+    /// Will lean on the default mapper if possible, and fall back to mapping via the type schema
     /// </summary>
-    /// <param name="completion"></param>
-    /// <param name="parsed"></param>
-    /// <param name="error"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="completion">The completion to get the result from</param>
+    /// <param name="parsed">The parsed result</param>
+    /// <param name="error">The error if the result cannot be parsed</param>
+    /// <returns>True if the result can be parsed, false otherwise</returns>
     public static bool TryToGetResultWithTherapy<T>(this ChatCompletion<T> completion,
         [NotNullWhen(true)] out T? parsed,
         [NotNullWhen(false)] out string? error) where T : class
